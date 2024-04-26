230aba430a7283b7bbae0a32c05c242f58d9ef29
Tekin Zeiadin <tekinzeiadin@MacBook-Air-de-Tekin.local>
2024-04-26 23:05:25 +0200

    Printear ofertas de las 2 primeras paginas

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 from ScrapeJobOffers import scrapeWeb3Offers
 
-scrapeWeb3Offers()
+country ="Dubai"
+print(f"Ofertas en {country} {"Page 1"}: ")
+offers_page1 = scrapeWeb3Offers(country)
+for offer in offers_page1:
+    print(offer)
+    print()
+
+print(f"Offers in {country} {"Page 2"}: ")
+offers_page2=scrapeWeb3Offers(country)
+for offer in offers_page2:
+    print(offer)
+    print()
